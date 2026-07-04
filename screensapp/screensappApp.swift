47d9e031b77e4f2afad242952a27f48faf29cac7
evan <evanftjhin@gmail.com>
2026-07-04 14:22:26 +0800

    Initial Commit

---
+++ screensapp/screensappApp.swift
@@ -0,0 +1,17 @@
+//
+//  screensappApp.swift
+//  screensapp
+//
+//  Created by evan on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct screensappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
